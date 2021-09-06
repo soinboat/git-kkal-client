048884537bc4340445b568b68be9a694e38839a9
Chiwon <emphasize1@gmail.com>
2021-09-07 00:39:25 +0900

    feat: implement url input form static ui

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import styled from 'styled-components';
 import NavBar from '../components/layouts/NavBar';
 import BranchBar from '../components/layouts/BranchBar';
 import ContentBox from '../components/layouts/ContentBox';
@@ -13,8 +14,37 @@ export default function Landing() {
       </HeaderWrapper>
       <BodyWrapper>
         <BranchBar>Branch bar</BranchBar>
-        <ContentBox>Content Box</ContentBox>
+        <ContentBox>
+          <Form>
+            <UrlInput />
+            <Button>Enter Repo Url</Button>
+          </Form>
+        </ContentBox>
       </BodyWrapper>
     </>
   );
 }
+
+const Form = styled.form`
+  width: 50%;
+`;
+
+const UrlInput = styled.input`
+  width: 100%;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  color: #fff;
+  background-color: #343134;
+  border: none;
+  font-size: 18px;
+  height: 44px;
+  min-width: 100px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #171717;
+    border: none;
+  }
+`;
