9b2b573469ac80911fa3472f2ffe7ca518699a2e
Chiwon <emphasize1@gmail.com>
2021-09-08 15:10:29 +0900

    fix: correct key name to get fetched data
     - get /repository api updated
     - repoData.branchList -> repoData.logList

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 const getBranchList = (repoData) => {
-  const branchList = repoData.branchList.map((log) => log.branchName2);
+  const branchList = repoData.logList.map((log) => log.branchName2);
   return branchList.filter(
     (branch, index) => branchList.indexOf(branch) === index,
   );
