acbe2e75d20192837f0ff80aa814e4ca7f39c2c8
Sunkwan Kim <skyvlu@gmail.com>
2021-09-10 06:14:57 +0900

    feat: implement diff component that show targetDiff changed logs

---
+++ src/components/layouts/Diff.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+export default function Diff({ targetDiff }) {
+  console.log(targetDiff);
+  return (
+    <Wrapper>
+      <DiffFileName>{targetDiff.fileName}</DiffFileName>
+      {targetDiff.changedLog.map((log) => (
+        <InnerWrapper>
+          <DiffTitle>
+            {`${log.codeLineOffsetString} ${log.codeBeginHunk}`}
+          </DiffTitle>
+          <div>{JSON.stringify(log)}</div>
+        </InnerWrapper>
+      ))}
+    </Wrapper>
+  );
+}
+
+const DiffFileName = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  width: 100%;
+  height: 40px;
+  border-top: 1px solid ${({ theme }) => theme.FONT.GERY};
+  background-color: ${({ theme }) => theme.BACKGROUND.BLACK};
+  color: ${({ theme }) => theme.FONT.GREY};
+`;
+
+const InnerWrapper = styled.div`
+  margin: 10px;
+`;
+
+const DiffTitle = styled.div``;
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+`;
+
+Diff.defaultProps = {
+  targetDiff: {
+    before: {
+      line: 0,
+      logList: [],
+    },
+    after: {
+      line: 0,
+      logList: [],
+    },
+    codeBeginHunk: '',
+  },
+};
+
+Diff.propTypes = {
+  targetDiff: PropTypes.objectOf(
+    PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.arrayOf(
+        PropTypes.objectOf(
+          PropTypes.oneOfType([
+            PropTypes.string,
+            PropTypes.objectOf(
+              PropTypes.oneOfType([
+                PropTypes.number,
+                PropTypes.arrayOf(PropTypes.string),
+              ]),
+            ),
+          ]),
+        ),
+      ),
+    ]),
+  ),
+};
