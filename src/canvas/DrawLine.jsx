764871075f78bb623dc8783c289af8f380fbca22
seunghyun oh <oshyun6623@gmail.com>
2021-09-09 15:44:36 +0900

    fix: duplicate render

--- src/canvas/DrawLine.jsx
+++ src/canvas/DrawLine.jsx
@@ -5,56 +5,59 @@ import { Graphics } from '@inlet/react-pixi';
 import convertColor from '../utils/convertColor';
 
 export default function DrawLine({ logList }) {
-  const draw = useCallback((graph) => {
-    const lindData = [];
-    const SLOPE = 20;
-    graph.clear();
+  const draw = useCallback(
+    (graph) => {
+      const lindData = [];
+      const SLOPE = 20;
+      graph.clear();
 
-    const nodePositionData = logList.map((log, index) => {
-      const circlePosition = {
-        x: log.position * 100,
-        y: index * 50 + 100,
-      };
+      const nodePositionData = logList.map((log, index) => {
+        const circlePosition = {
+          x: log.position * 100,
+          y: index * 50 + 100,
+        };
 
-      return circlePosition;
-    });
-
-    logList.forEach((log, index) => {
-      log.parents.forEach((parent) => {
-        const parentIndex = logList.findIndex(
-          (targetLog) => targetLog.hash === parent,
-        );
-        const color =
-          log.position > logList[parentIndex].position
-            ? log.color
-            : logList[parentIndex].color;
-        lindData.push({ start: index, to: parentIndex, color });
+        return circlePosition;
       });
-    });
 
-    lindData.forEach((line) => {
-      const startPoint = nodePositionData[line.start];
-      const endPoint = nodePositionData[line.to];
-      const circleColor = convertColor(line.color);
-      graph.lineStyle(2, circleColor);
+      logList.forEach((log, index) => {
+        log.parents.forEach((parent) => {
+          const parentIndex = logList.findIndex(
+            (targetLog) => targetLog.hash === parent,
+          );
+          const color =
+            log.position > logList[parentIndex].position
+              ? log.color
+              : logList[parentIndex].color;
+          lindData.push({ start: index, to: parentIndex, color });
+        });
+      });
 
-      if (startPoint.x > endPoint.x) {
-        graph.moveTo(startPoint.x, startPoint.y);
-        graph.lineTo(startPoint.x, endPoint.y - SLOPE);
-        graph.moveTo(startPoint.x, endPoint.y - 20);
-        graph.lineTo(endPoint.x, endPoint.y);
-      } else if (startPoint.x < endPoint.x) {
-        graph.moveTo(startPoint.x, startPoint.y);
-        graph.lineTo(endPoint.x, startPoint.y + 20);
-        graph.moveTo(endPoint.x, startPoint.y + 20);
-        graph.lineTo(endPoint.x, endPoint.y);
-      } else {
-        graph.moveTo(startPoint.x, startPoint.y);
-        graph.lineTo(endPoint.x, endPoint.y);
-      }
-    });
-    graph.endFill();
-  }, []);
+      lindData.forEach((line) => {
+        const startPoint = nodePositionData[line.start];
+        const endPoint = nodePositionData[line.to];
+        const circleColor = convertColor(line.color);
+        graph.lineStyle(2, circleColor);
+
+        if (startPoint.x > endPoint.x) {
+          graph.moveTo(startPoint.x, startPoint.y);
+          graph.lineTo(startPoint.x, endPoint.y - SLOPE);
+          graph.moveTo(startPoint.x, endPoint.y - 20);
+          graph.lineTo(endPoint.x, endPoint.y);
+        } else if (startPoint.x < endPoint.x) {
+          graph.moveTo(startPoint.x, startPoint.y);
+          graph.lineTo(endPoint.x, startPoint.y + 20);
+          graph.moveTo(endPoint.x, startPoint.y + 20);
+          graph.lineTo(endPoint.x, endPoint.y);
+        } else {
+          graph.moveTo(startPoint.x, startPoint.y);
+          graph.lineTo(endPoint.x, endPoint.y);
+        }
+      });
+      graph.endFill();
+    },
+    [logList],
+  );
   // return <div></div>
   return <Graphics draw={draw} />;
 }
--- src/components/Graph2d.jsx
+++ src/components/Graph2d.jsx
@@ -1,34 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Stage } from '@inlet/react-pixi';
+import styled from 'styled-components';
+
 import useWindowDimensions from '../hooks/useWindowDimensions';
 
 import DrawLine from '../canvas/DrawLine';
 import DrawNode from '../canvas/DrawNode';
 
 export default function Graph2d({ repoData }) {
+  console.log('렌더 횟수');
   if (repoData.repoName === 'repoName') {
     return <div>데이터없음</div>;
   }
 
   const { logList } = repoData;
 
-  const { width, height } = useWindowDimensions();
-  const STAGE_SIZE = height - 60;
+  const { width } = useWindowDimensions();
+  // const STAGE_SIZE = height - 60;
 
   return (
-    <Stage
-      width={width - 467 > 200 ? width - 467 : 200}
-      height={STAGE_SIZE}
-      options={{ backgroundColor: 0xffffff }}
-    >
-      <DrawLine logList={logList} />
-      {logList.map((log, index) => (
-        <DrawNode key={log.hash} log={log} index={index} />
-      ))}
-    </Stage>
+    <Wrapper>
+      <Stage
+        width={width - 467 > 200 ? width - 467 : 200}
+        height={150 + logList.length * 50}
+        options={{ backgroundColor: 0xffffff }}
+      >
+        <DrawLine logList={logList} />
+        {logList.map((log, index) => (
+          <DrawNode key={log.hash} log={log} index={index} />
+        ))}
+      </Stage>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  overflow-x: hidden;
+`;
 
 Graph2d.defaultProps = {
   repoData: {
