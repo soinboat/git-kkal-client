c140bc743e61e8e0453e0c68f5cc72771efcb7c1
seunghyun oh <oshyun6623@gmail.com>
2021-09-13 20:43:39 +0900

    fix: change constant var name

--- src/components/layouts/ContentBox.jsx
+++ src/components/layouts/ContentBox.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { CONTENT_BOX_WIDTH } from '../../constants/size';
+import { CONTENT_BOX_MIN_WIDTH } from '../../constants/size';
 
 export default function ContentBox({ children }) {
   return <Wrapper>{children || null}</Wrapper>;
@@ -11,7 +11,7 @@ export default function ContentBox({ children }) {
 const Wrapper = styled.div`
   display: flex;
   width: 100%;
-  min-width: ${CONTENT_BOX_WIDTH};
+  min-width: ${CONTENT_BOX_MIN_WIDTH};
   height: 100%;
   justify-content: center;
   align-items: center;
