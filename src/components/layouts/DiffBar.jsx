e6b5575d2739fe96028142bb907e529e913c9245
Sunkwan Kim <skyvlu@gmail.com>
2021-09-16 18:08:16 +0900

    refactor: fix branch bar layout  and refactor codes

--- src/components/layouts/DiffBar.jsx
+++ src/components/layouts/DiffBar.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import theme from '../../context/theme';
-
 export default function DiffBar({ children }) {
   return <Wrapper>{children || null}</Wrapper>;
 }
 
 const Wrapper = styled.div`
-  min-width: ${theme.size.diffBarWidth};
-  width: 25%;
+  min-width: ${({ theme: { size } }) => size.diffBarMinWidth};
+  width: ${({ theme: { size } }) => size.diffBarWidth};
   height: 100%;
   background-color: ${({ theme: { background } }) => background.grey3};
   color: ${({ theme: { font } }) => font.color.white};
--- src/components/layouts/NavBar.jsx
+++ src/components/layouts/NavBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import theme from '../../context/theme';
 
 export default function NavBar({ children }) {
   return <Wrapper>{children}</Wrapper>;
@@ -9,7 +8,7 @@ export default function NavBar({ children }) {
 
 const Wrapper = styled.nav`
   width: 100%;
-  height: ${theme.size.navBarHeight};
+  height: ${({ theme: { size } }) => size.navBarHeight};
   background-color: ${({ theme: { background } }) => background.black};
   color: ${({ theme: { font } }) => font.color.grey};
 `;
