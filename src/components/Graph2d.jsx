56dc847deecaeaf7795cca79e947f08f699b9f4e
Sunkwan Kim <skyvlu@gmail.com>
2021-09-17 21:42:17 +0900

    feat: implement sub nav bar

--- src/components/Graph2d.jsx
+++ src/components/Graph2d.jsx
@@ -21,28 +21,64 @@ export default function Graph2d({ repoData, targetCommit, handleNodeClick }) {
   };
 
   return (
-    <GraphWrapper>
-      <DrawGraph
-        logList={limitedLogList}
-        lineList={limitedLineList.flat()}
-        targetCommit={targetCommit}
-        maxPipeCount={maxPipeCount}
-        onClickHandler={onClickHandler}
-      />
-      <Description
-        logList={limitedLogList}
-        targetCommit={targetCommit}
-        onClickHandler={onClickHandler}
-      />
-    </GraphWrapper>
+    <Graph2dWrapper>
+      <NavBar>
+        <Item
+          width={`${(maxPipeCount + 1) * theme.size.graph2dNodeSpacing}px`}
+          borderRight
+        >
+          GRAPH
+        </Item>
+        <Item borderRight>HASH</Item>
+        <Item>COMMIT</Item>
+      </NavBar>
+      <Content>
+        <DrawGraph
+          logList={limitedLogList}
+          lineList={limitedLineList.flat()}
+          targetCommit={targetCommit}
+          maxPipeCount={maxPipeCount}
+          onClickHandler={onClickHandler}
+        />
+        <Description
+          logList={limitedLogList}
+          targetCommit={targetCommit}
+          onClickHandler={onClickHandler}
+        />
+      </Content>
+    </Graph2dWrapper>
   );
 }
 
-const GraphWrapper = styled.div`
+const Graph2dWrapper = styled.div`
   width: 100%;
-  display: flex;
   height: 100%;
+`;
+
+const NavBar = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 30px;
+  background-color: ${(props) => props.theme.background.grey2};
+  color: ${(props) => props.theme.font.grey};
+`;
+
+const Item = styled.div`
+  width: ${({ width }) => width || '100px'};
+  margin-left: 10px;
+  border-right: ${(props) => (props.borderRight ? `2px solid grey` : '')};
+  font-size: 0.8rem;
+  box-sizing: border-box;
+`;
+
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  /* height: 100%; */
+  height: calc(100% - 60px);
   overflow: scroll;
+  /* border: 1px solid yellow; */
 `;
 
 Graph2d.defaultProps = {
